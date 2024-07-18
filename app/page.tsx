3cd5c5b9e7e4f015ccc8dac5c7038867561adf31
Rajat Chaudhary <rajatchaudhary@Rajats-MacBook-Air.local>
2024-07-18 14:10:27 +0530

    feat: setup done

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Text } from "@/components/ui/text";
+
+export default function Home() {
+  return (
+    <main>
+      <Text className="text-red-500">hello world</Text>
+    </main>
+  );
+}
